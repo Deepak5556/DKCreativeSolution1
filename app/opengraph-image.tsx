66f512b3ec0b17bf54b2dcedfdf920367c3932d6
Deepak5556 <deepakviji5556@gmail.com>
2026-06-22 22:57:58 +0530

    IC

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -3,6 +3,7 @@ import { siteConfig } from "@/lib/constants";
 
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
+export const dynamic = "force-static";
 
 export default function OpengraphImage() {
   return new ImageResponse(
